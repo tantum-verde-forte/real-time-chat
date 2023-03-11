7ea28fa48e3a0570fd31f2cde72f55abb4e2bdbe
frees <troshin-aleksey@bk.ru>
2023-03-12 02:54:51 +0300

    Added profile model

--- chat/chat_api/models.py
+++ chat/chat_api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+from django.contrib.auth.models import AbstractUser
 
 class Room(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False, unique=True)
@@ -14,3 +14,7 @@ class Message(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="messages")
 
+class Profile(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    profile_name = models.CharField(max_length=30)
+    profile_image = models.ImageField(upload_to='images/', max_length=100, default=None)
